519069423b02bd623584c91b5391c6243fa59e03
KyuSahm <gusami32@gmail.com>
2021-08-12 21:36:22 +0900

    tile view applied

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -2,12 +2,25 @@ package com.newlecture.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
 	
-	@RequestMapping("/index")
-	public String index() {
-		return "Hello Spring Boot";
+	@RequestMapping("index")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("home.index");
+		mv.addObject("data", "Hello Spring Boot");
+				
+		return mv;
+	}
+	
+	@RequestMapping("help")
+	public ModelAndView help() {
+		ModelAndView mv = new ModelAndView("home.help");
+		mv.addObject("data", "I can help you");
+				
+		return mv;
 	}
 }
--- src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -3,20 +3,23 @@ package com.newlecture.web.controller.customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/customer/notice/")
 public class NoticeController {
 	// class Request Mapping + method Request Mapping
 	@RequestMapping("list")
-	public String list(Model model) {
-		model.addAttribute("test", "Hello~");		
-		return "customer/notice/list";
+	public ModelAndView list(Model model) {
+		ModelAndView mv = new ModelAndView("customer.notice.list");
+		mv.addObject("test", "Hello~");
+		return mv;
 	}
 	
 	// class Request Mapping + method Request Mapping
 	@RequestMapping("detail")
-	public String detail() {
-		return "customer/notice/detail";
+	public ModelAndView detail() {
+		ModelAndView mv = new ModelAndView("customer.notice.detail");
+		return mv;
 	}
 }
